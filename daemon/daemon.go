5bb09f12cb819280b451fbbf88006837a06f842b
Petros Angelatos <petrosagg@gmail.com>
2014-12-05 02:45:27 +0000

    Remove architecture check

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -1113,10 +1113,6 @@ func (daemon *Daemon) ImageGetCached(imgID string, config *runconfig.Config) (*i
 }
 
 func checkKernelAndArch() error {
-	// Check for unsupported architectures
-	if runtime.GOARCH != "amd64" {
-		return fmt.Errorf("The Docker runtime currently only supports amd64 (not %s). This will change in the future. Aborting.", runtime.GOARCH)
-	}
 	// Check for unsupported kernel versions
 	// FIXME: it would be cleaner to not test for specific versions, but rather
 	// test for specific functionalities.
